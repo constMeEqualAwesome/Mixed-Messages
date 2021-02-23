1191c2bd95256fa8840dc077a610b54a979617d5
Ryan Tomich <79169350+constMeEqualAwesome@users.noreply.github.com>
2021-02-23 16:49:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,23 @@ let messageGroup2 = ['eat', 'smell', 'take', 'have', 'steal', 'cook', 'use', 'tr
 let messageGroup3 = ['a person', 'a cat', 'a house', 'a bath', 'anyone', 'some cheese', 'that guy', 'my chickens', 'them'];
 
 let questionMessageGroup = ['Why don\'t', 'Couldn\'t', 'Will', 'Can', 'Might', 'Why can\'t', 'Could', 'Wont'];
+// Defines the message groups the function will choose from
 
 function returnMessage () {
   let randomNum = Math.floor(Math.random() * 2);
+  // Generates a random number to decide weather or not to ask a question, or make a statement
   let message = [];
+  // Defines an empty message
   if (randomNum === 1) {
     message = [['You'], messageGroup1[Math.floor(Math.random() * 8)], messageGroup2[Math.floor(Math.random() * 9)], `${messageGroup3[Math.floor(Math.random() * 9)]}!`];
+    // Assigns a value to the message
   } else {
     message = [questionMessageGroup[Math.floor(Math.random() * 8)], ['you'], messageGroup2[Math.floor(Math.random() * 9)], `${messageGroup3[Math.floor(Math.random() * 9)]}?`];
+    // Assigns a value to the message
   }
   return message.join(' ');
+  // Returns the message
 }
 
 console.log(returnMessage());
-
+// Logs the message
