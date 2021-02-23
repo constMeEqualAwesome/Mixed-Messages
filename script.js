b8a1cdbb10365d52536acd84b12b2d38e21f37e6
Ryan Tomich <79169350+constMeEqualAwesome@users.noreply.github.com>
2021-02-23 16:08:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ let messageGroup1 = ['should', 'shouldn\'t', 'will', 'might', 'can\'t', 'can', '
 let messageGroup2 = ['eat', 'smell', 'take', 'have', 'steal', 'cook', 'use', 'trust'];
 let messageGroup3 = ['a person', 'a cat', 'a house', 'a bath', 'anyone', 'some cheese', 'that guy', 'my chickens'];
 
-function logMessage () {
+function returnMessage () {
   let message = [messageGroup1[Math.floor(Math.random() * 9)], messageGroup2[Math.floor(Math.random() * 9)], messageGroup3[Math.floor(Math.random() * 9)]]
   return message.join(' ');
 }
 
-logMessage();
+console.log(returnMessage());
+
